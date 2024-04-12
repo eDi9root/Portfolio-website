a94c591e8b9ba6774c9b1e5934db3513dcbaf03f
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-04-12 01:57:16 -0400

    updated

--- src/pages/Design/MobileHeader.jsx
+++ src/pages/Design/MobileHeader.jsx
@@ -4,7 +4,3 @@ export const MobileMenu = () => {
     <div className="absolute inset-0 pointer-events-none lg:hidden">
       <div className="absolute inset-0 opacity-[.03]">
         
-      </div>
-    </div>
-  );
-};
